1371efb825518fb171ac9a8a6164004d051754d2
Nathan VIAUD <nathan.viaud@iut2.univ-grenoble-alpes.fr>
2021-02-09 17:25:37 +0100

    cadre de l'écran

--- Jeu/fond.py
+++ Jeu/fond.py
@@ -1,10 +1,12 @@
 import pygame
 
 class fond:
-    def __init__(self, img, screen):
+    def __init__(self, img, screen, taille):
         self.img = img
         self.pos_x= 0
         self.screen = screen
+        self.taille = taille
+
 
     def update(self):
         self.screen.blit(self.img, (self.pos_x, 0))
