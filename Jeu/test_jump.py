8de0fae319b3028f511c179bd97f270622dd66af
Abel LAROUSSI <abel.laroussi@etu.univ-grenoble-alpes.fr>
2021-02-09 22:24:01 +0100

    Déplacement de genTuto()

--- Jeu/test_jump.py
+++ Jeu/test_jump.py
@@ -13,6 +13,49 @@ from obstacles import *
 
 pygame.init()
 
+def genTuto():
+
+        # Sol du terrain
+        sol = plateforme(0, 750, screen, 8196, 18)
+        p_saut1 = plateforme(1300, 600, screen, 100, 10)
+        p_saut2 = plateforme(1600, 450, screen, 100, 10)
+        p_saut3 = plateforme(1900, 600, screen, 100, 10)
+        p_saut4 = plateforme(2400,700, screen, 50, 50)
+        p_saut5 = plateforme(2662,700, screen, 50, 50)
+        plateformes = []
+        plateformes.append(sol)
+        plateformes.append(p_saut1)
+        plateformes.append(p_saut2)
+        plateformes.append(p_saut3)
+        plateformes.append(p_saut4)
+        plateformes.append(p_saut5)
+
+        # Fruits du niveau:
+        fruits = []
+        f1 = fruit(800, 600, screen)
+        f2 = fruit(1100, 400, screen)
+        fruits.append(f1)
+        fruits.append(f2)
+
+        # Enemis:
+        ennemis = []
+       # e1 = ennemi(2, 900, 350, screen, 900, 900, 1, 250, 500, 1)
+       # ennemis.append(e1)
+
+        # Piques ( A changer)
+        obs = []
+        obstacle1 = obstacles(2460, 690, screen)
+        obstacle2 = obstacles(2524, 690, screen)
+        obstacle3 = obstacles(2588, 690, screen)
+        obs.append(obstacle1)
+        obs.append(obstacle2)
+        obs.append(obstacle3)
+
+        #clé
+        cle1 = cle(1400, 400, screen)
+        fond.pos_x = 0
+        return world(fond, plateformes, fruits, ennemis, obs, cle1)
+
 VITESSE_SAUT = 40
 HAUTEUR_SAUT = 100
 
@@ -59,47 +102,6 @@ white = (255,255,255)
 font = pygame.font.Font(None, 50)
 mouvement_tuto = font.render("Utilisez les fleches directionnelles pour bouger", True, white)
 saut = font.render("Pour sauter utilisez la barre espace", True, white)
-def genTuto():
-        # Sol du terrain
-        sol = plateforme(0, 750, screen, 8196, 18)
-        p_saut1 = plateforme(1300, 600, screen, 100, 10)
-        p_saut2 = plateforme(1600, 450, screen, 100, 10)
-        p_saut3 = plateforme(1900, 600, screen, 100, 10)
-        p_saut4 = plateforme(2400,700, screen, 50, 50)
-        p_saut5 = plateforme(2662,700, screen, 50, 50)
-        plateformes = []
-        plateformes.append(sol)
-        plateformes.append(p_saut1)
-        plateformes.append(p_saut2)
-        plateformes.append(p_saut3)
-        plateformes.append(p_saut4)
-        plateformes.append(p_saut5)
-
-        # Fruits du niveau:
-        fruits = []
-        f1 = fruit(800, 600, screen)
-        f2 = fruit(1100, 400, screen)
-        fruits.append(f1)
-        fruits.append(f2)
-
-        # Enemis:
-        ennemis = []
-       # e1 = ennemi(2, 900, 350, screen, 900, 900, 1, 250, 500, 1)
-      #  ennemis.append(e1)
-
-        # Piques ( A changer)
-        obs = []
-        obstacle1 = obstacles(2460, 690, screen)
-        obstacle2 = obstacles(2524, 690, screen)
-        obstacle3 = obstacles(2588, 690, screen)
-        obs.append(obstacle1)
-        obs.append(obstacle2)
-        obs.append(obstacle3)
-
-        #clé
-        cle1 = cle(1400, 400, screen)
-
-        return world(fond, plateformes, fruits, ennemis, obs, cle1)
 
 tuto = genTuto()
 # Joueur:
