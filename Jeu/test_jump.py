f6030a86a0844157b9536c942e2044a7088d9c5b
TinTinTounsi <TinTinTounsi.Pro@gmail.com>
2021-02-10 12:37:20 +0100

    fix fond

--- Jeu/test_jump.py
+++ Jeu/test_jump.py
@@ -15,81 +15,79 @@ from sortie import *
 
 pygame.init()
 
-plateformes = []
-fruits = []
-ennemis = []
-obs = []
 
 def genTuto():
-        from ennemi import ennemi
-        plateformes.clear()
-        fruits.clear()
-        ennemis.clear()
-        obs.clear()
-        # Sol du terrain
-        sol = plateforme(0, 750, screen, 8196, 18, "sol")
-        p_saut1 = plateforme(1300, 600, screen, 100, 10, "plat")
-        p_saut2 = plateforme(1600, 450, screen, 100, 10, "plat")
-        p_saut3 = plateforme(1900, 600, screen, 100, 10, "plat")
-        p_saut4 = plateforme(2400,700, screen, 50, 50, "plat")
-        p_saut5 = plateforme(2662,700, screen, 50, 50, "plat")
-        p_sautfruit1 = plateforme(3000, 600, screen, 100, 10, "plat")
-        p_sautfruit2 = plateforme(3200, 450, screen, 100, 10, "plat")
-        p_sautfruit3 = plateforme(3400, 250, screen, 100, 10, "plat")
-        p_porte = plateforme(4000, 0, screen, 100,768, "porte")
-        p_cle1 = plateforme(4250, 600, screen, 100, 10, "plat")
-        p_cle2 = plateforme(4600, 450, screen, 100, 10, "plat")
-        p_cle3 = plateforme(4800, 450, screen, 300, 10, "plat")
-        p_cle4 = plateforme(5250,450, screen, 100, 10, "plat")
-        plateformes.append(sol)
-        plateformes.append(p_saut1)
-        plateformes.append(p_saut2)
-        plateformes.append(p_saut3)
-        plateformes.append(p_saut4)
-        plateformes.append(p_saut5)
-        plateformes.append(p_sautfruit1)
-        plateformes.append(p_sautfruit2)
-        plateformes.append(p_sautfruit3)
-        plateformes.append(p_porte)
-        plateformes.append(p_cle1)
-        plateformes.append(p_cle2)
-        plateformes.append(p_cle3)
-        plateformes.append(p_cle4)
+    from fond import fond
+    from ennemi import ennemi
+    fond = fond(screen, 8196)
+    # Sol du terrain
+    plateformes = []
+    sol = plateforme(0, 750, screen, 8196, 18, "sol")
+    p_saut1 = plateforme(1300, 600, screen, 100, 10, "plat")
+    p_saut2 = plateforme(1600, 450, screen, 100, 10, "plat")
+    p_saut3 = plateforme(1900, 600, screen, 100, 10, "plat")
+    p_saut4 = plateforme(2400,700, screen, 50, 50, "plat")
+    p_saut5 = plateforme(2662,700, screen, 50, 50, "plat")
+    p_sautfruit1 = plateforme(3000, 600, screen, 100, 10, "plat")
+    p_sautfruit2 = plateforme(3200, 450, screen, 100, 10, "plat")
+    p_sautfruit3 = plateforme(3400, 250, screen, 100, 10, "plat")
+    p_porte = plateforme(4000, 0, screen, 100,768, "porte")
+    p_cle1 = plateforme(4250, 600, screen, 100, 10, "plat")
+    p_cle2 = plateforme(4600, 450, screen, 100, 10, "plat")
+    p_cle3 = plateforme(4800, 450, screen, 300, 10, "plat")
+    p_cle4 = plateforme(5250,450, screen, 100, 10, "plat")
+    plateformes.append(sol)
+    plateformes.append(p_saut1)
+    plateformes.append(p_saut2)
+    plateformes.append(p_saut3)
+    plateformes.append(p_saut4)
+    plateformes.append(p_saut5)
+    plateformes.append(p_sautfruit1)
+    plateformes.append(p_sautfruit2)
+    plateformes.append(p_sautfruit3)
+    plateformes.append(p_porte)
+    plateformes.append(p_cle1)
+    plateformes.append(p_cle2)
+    plateformes.append(p_cle3)
+    plateformes.append(p_cle4)
 
-        # Fruits du niveau:
-        f1 = fruit(3035, 555, screen)
-        f2 = fruit(3235, 400, screen)
-        f3 = fruit(3435, 200, screen)
-        fruits.append(f1)
-        fruits.append(f2)
-        fruits.append(f3)
+    # Fruits du niveau:
+    fruits = []
+    f1 = fruit(3035, 555, screen)
+    f2 = fruit(3235, 400, screen)
+    f3 = fruit(3435, 200, screen)
+    fruits.append(f1)
+    fruits.append(f2)
+    fruits.append(f3)
 
-        # Enemis:
-        e1 = ennemi(2, 4470, 500, screen, 4470, 4470, 1, 300, 650, 2)
-        e2 = ennemi(1, 4800, 350, screen, 4799, 5050, 2, 350, 350, 1)
-        ennemis.append(e1)
-        ennemis.append(e2)
+    # Enemis:
+    e1 = ennemi(2, 4470, 500, screen, 4470, 4470, 1, 300, 650, 2)
+    e2 = ennemi(1, 4800, 350, screen, 4799, 5050, 2, 350, 350, 1)
+    ennemis = []
+    ennemis.append(e1)
+    ennemis.append(e2)
 
-        # Piques ( A changer)
-        obstacle1 = obstacles(2460, 690, screen)
-        obstacle2 = obstacles(2524, 690, screen)
-        obstacle3 = obstacles(2588, 690, screen)
-        obs.append(obstacle1)
-        obs.append(obstacle2)
-        obs.append(obstacle3)
+    # Piques ( A changer)
+    obstacle1 = obstacles(2460, 690, screen)
+    obstacle2 = obstacles(2524, 690, screen)
+    obstacle3 = obstacles(2588, 690, screen)
+    obs = []
+    obs.append(obstacle1)
+    obs.append(obstacle2)
+    obs.append(obstacle3)
 
-        #clé
-        cle1 = cle(5280, 400, screen)
+    #clé
+    cle1 = cle(5280, 400, screen)
 
-        #lit
-        l1 = lit(3800, 700, screen)
+    #lit
+    l1 = lit(3800, 700, screen)
 
-        # Sortie
-        sort = sortie(5750, 622, screen)
+    # Sortie
+    sort = sortie(5750, 622, screen)
 
-        fond.reset()
+    fond.reset()
 
-        return world(fond, plateformes, fruits, ennemis, obs, cle1, l1, sort)
+    return world(fond, plateformes, fruits, ennemis, obs, cle1, l1, sort)
 
 VITESSE_SAUT = 40
 HAUTEUR_SAUT = 100
@@ -97,6 +95,8 @@ HAUTEUR_SAUT = 100
 size = (1024,768)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("cours forest")
+
+tuto = genTuto()
 
 joue = True
 
@@ -106,7 +106,6 @@ enSaut = False
 mort = pygame.image.load("images/mort.png")
 course = pygame.mixer.Sound("./son/course.wav")
 saut = pygame.mixer.Sound("./son/saut.wav")
-fond = fond(screen, 8196)
 
 #p_saut1 = plateforme(450, 450, screen)
 #p_saut2 = plateforme(750, 550, screen)
@@ -138,9 +137,6 @@ font = pygame.font.Font(None, 50)
 mouvement_tuto = font.render("Utilisez les fleches directionnelles pour bouger", True, white)
 saut = font.render("Pour sauter utilisez la barre espace", True, white)
 fruits_message = font.render("Prenez les fruit pour augmenter votre score !", True, white)
-
-
-tuto = genTuto()
 # Joueur:
 joueur = player(3, 600, screen, tuto)
 
@@ -162,9 +158,9 @@ while monde:
         joueur.deplaceAnimation()
         joueur.update()
         if tutorial == True:
-            screen.blit(mouvement_tuto, (fond.pos_x+50, 200))
-            screen.blit(saut, (fond.pos_x +1200,200))
-            screen.blit(fruits_message, (fond.pos_x+2500, 200))
+            screen.blit(mouvement_tuto, (tuto.fond.pos_x+50, 200))
+            screen.blit(saut, (tuto.fond.pos_x +1200,200))
+            screen.blit(fruits_message, (tuto.fond.pos_x+2500, 200))
         pygame.display.update()
 
         while joueur.mort and joue:
