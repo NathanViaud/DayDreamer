8f9335d020b2938ef90faf1caa6f7a4dcc498e9c
Abel LAROUSSI <abel.laroussi@etu.univ-grenoble-alpes.fr>
2021-02-12 03:31:56 +0100

    ZZZZZZZZZZZZZZZZZZZZZZZZ

--- Jeu/test_jump.py
+++ Jeu/test_jump.py
@@ -73,7 +73,7 @@ def runGame(screen):
     niveaux.append(niveau3)
     niveaux.append(niveau4)
 
-    level = 4
+    level = 0
     pts = 0
     cmpt_mort = 0
     tete_mort = pygame.image.load("sprites/compteur_mort.png")
